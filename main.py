35718122556e451c0e3c21fda81472a3b9223ef1
MaccasAU <51145706+MaccasAU@users.noreply.github.com>
2020-06-03 11:06:07 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+import sys
+import os
+import random
+import pickle
+
+#following section of code courtesy of David Ng
+#remove '#' for awesomeness
+#for i in range(2000000000000):
+  #print("Cow!!!!!🐄")
+
+
+#def print(str):
+    #for letter in str:
+        #sys.stdout.write(letter)
+        #sys.stdout.flush()
+        #time.sleep(0.09)
+
+#didn't work well# #answer_A = ["A", "a"] answer_B = ["B", "b"]
+
+print("▒█░░▒█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ▀▀█▀▀ █▀▀█ 　 █▀▄▀█ █░░█ 　 █▀▀▀ █▀▀█ █▀▄▀█ ▀▀▒█▒█▒█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ 　 ░░█░░ █░░█ 　 █░▀░█ █▄▄█ 　 █░▀█ █▄▄█ █░▀░█ █▀▀ ▒█▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ 　 ░░▀░░ ▀▀▀▀ 　 ▀░░░▀ ▄▄▄█ 　 ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ █░░▒█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ▀▀█▀▀ █▀▀█ 　 █▀▄▀█ █░░█ 　 █▀▀▀ █▀▀█ █▀▄▀█ ▀▀▒█▒█▒█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ 　 ░░█░░ █░░█ 　 █░▀░█ █▄▄█ 　 █░▀█ █▄▄█ █░▀░█ █▀▀ ▒█▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ 　 ░░▀░░ ▀▀▀▀ 　 ▀░░░▀ ▄▄▄█ 　 ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ █░░▒█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ▀▀█▀▀ █▀▀█ 　 █▀▄▀█ █░░█ 　 █▀▀▀ █▀▀█ █▀▄▀█ ▀▀▒█▒█▒█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ 　 ░░█░░ █░░█ 　 █░▀░█ █▄▄█ 　 █░▀█ █▄▄█ █░▀░█ █▀▀ ▒█▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ 　 ░░▀░░ ▀▀▀▀ 　 ▀░░░▀ ▄▄▄█ 　 ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ █░░▒█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ▀▀█▀▀ █▀▀█ 　 █▀▄▀█ █░░█ 　 █▀▀▀ █▀▀█ █▀▄▀█ ▀▀▒█▒█▒█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ 　 ░░█░░ █░░█ 　 █░▀░█ █▄▄█ 　 █░▀█ █▄▄█ █░▀░█ █▀▀ ▒█▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ 　 ░░▀░░ ▀▀▀▀ 　 ▀░░░▀ ▄▄▄█ 　 ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ ")
+print("   ᵂᵉˡᶜᵒᵐᵉ ᵗᵒ ᴶᵒⁿᵃᵗʰᵃⁿ'ˢ ᵍᵃᵐᵉ! 🅣🅗🅘🅢 🅟🅡🅞🅙🅔🅒🅣 🅦🅐🅢 🅕🅞🅡 🅢🅒🅗🅞🅞🅛 \n 🅼 🆈 🅶 🅸 🆃 🅷 🆄 🅱  🅸 🆂  @ 🅼 🅰 🅲 🅲 🅰 🆂 🅰 🆄  \n ʸᵒᵘ ᵃʳᵉ ʷᵉˡᶜᵒᵐᵉ ᵗᵒ ᶠᵒʳᵏ ᵐʸ ᵗʰⁱⁿᵍ.\n \n")
+#print("When given options for what to do, press enter, type your answer and press enter again.  \n \n")
+
+time.sleep(2)
+
+#Epic Gamer Moment#
+#*starts*#
+print("Sorry for the plot, it is not very good. \n \n \n \n")
+
+#source for story: https://www.write4fun.net/view-entry/205506
+
+def print(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.07)
+
+print ("After a long hard day at your boring office job you arrive at home. Before you walk into your house you see a letter in your mailbox, an unusual occurrence as mail day is usually Thursday not Monday. \n \n ")
+time.sleep(2)
+print("You go up to the mailbox and see that there is a little red letter, quite unusual.\n \n") 
+time.sleep(1) 
+print("You will: \n \n")
+print("A. Open the letter \n\
+B. Go inside. \n")
+choice = input(">>> ")
+#didn't work - val = int(answer_A)
+if choice == "a" or choice == "A":
+  print("You are too curious not to check the letter. So you grab it out of your mailbox and open it and find seven words.") 
+  time.sleep(1)
+  print("‘Meet me on the docks at twelve’. You are not that sure whether to go or not. It sounds very sketchy, but you feel like it. Despite it's obviously being a bad idea, you choose to go anyway.")
+if choice == "b" or choice == "B":
+  print("You decide that it is too suspicious and you choose to go inside. Half way up the drive you slip and fall head first on a rock. \n \nNo one knows what happened to your body. Did the writer of the letter dispose of it? \n \n \n \n \n THE END")
+  quit()
+print ("\n \nYou decide to go down to the docks to confront the person who wrote the red letter.")
+print(" 11:59 pm and there is no one there. Where is he? You think to yourself, did they forget? Is it a prank? You realise that was probably just a dumb joke.")
+time.sleep(2.5)
+print(" Then you hear a car rumble in the distance. A pair of bright yellow headlights pop into existence and they are heading towards you and the noise is getting louder.")
+time.sleep(0.5)
+print(" You have two options. \n \n A. Stand your ground. \n \n B. Run away if you feel threatened. You'll stay for now, but be ready to run \n")
+choice = input(">>> ")
+if choice == "a" or choice == "A":
+  print ("You decide to stand your ground. The car is getting dangerously close to you. Five seconds feels like it has been an hour. \n The car is getting nearer and you are regretting your decision. You can now see the red hood of the car coming towards you. \n Just as you get ready for the numbing pain of the car hitting you it screeches to a stop. A suited man comes out of the car and he is holding a hand gun. “Where is the money?” the man shouts at you.")
+  time.sleep(2) #...wait a sec to be tense...#
+  print(" You have no choice, but to run. You hear him shooting at you as you run, but miraculously he misses you ") 
+  print("You decide to run away. In the distance you hear a car. Its headlights are getting closer. You take a left turn into a dark alleyway and end up behind a small boat house. You see the car speed past you and you decide to run for the police.  \n ")
+  time.sleep(0.5)
+  print("You have two options. \n \n A. Run to your right \n \n B. Run to your left \n")
+  choice = input(">>> ")
+#haha you don't actually get to choose. epic prank lol 
+  print("You run down the road. You think you've dodged the car. It only took you ten minutes to find the police station nearest to you.  \n Once you arrive you quickly tell your story. Thirty minutes later the police sergeant returns looking happy with himself. He explains to you that you were the bait in a drug bust and now you are a key person in the crime of the year. \n This is bizarre, and as you question the ethics of this, you are escorted out of the police station. At least you didn't get hurt. You go home and watch Black Mirror. The End")
+  time.sleep(2)
+  quit()
+if choice == "b" or choice == "B":
+  time.sleep(0.5)
+  print ("You decide to stand your ground. The car is getting dangerously close to you. Five seconds feels like it has been an hour. \n The car is getting nearer and you are regretting your decision. You can now see the red hood of the car coming towards you. \n Just as you get ready for the numbing pain of the car hitting you it screeches to a stop. A suited man comes out of the car and he is holding a hand gun. “Where is the money?” the man shouts at you.")
+  time.sleep(2) #...wait a sec to be tense...#
+  print(" You have no choice, but to run. You hear him shooting at you as you run, but miraculously he misses you ") 
+  print("You decide to run away. In the distance you hear a car. Its headlights are getting closer. You take a left turn into a dark alleyway and end up behind a small boat house. You see the car speed past you and you decide to run for the police.  \n ")
+  time.sleep(0.5)
+  print("You have two options. \n \n A. Run to your right \n \n B. Run to your left \n")
+  choice = input(">>> ")
+#haha you don't actually get to choose. epic prank lol 
+  print("You run down the road. You think you've dodged the car. It only took you ten minutes to find the police station nearest to you.  \n Once you arrive you quickly tell your story. Thirty minutes later the police sergeant returns looking happy with himself. He explains to you that you were the bait in a drug bust and now you are a key person in the crime of the year. \n This is bizarre, and as you question the ethics of this, you are escorted out of the police station. At least you didn't get hurt. You go home and watch Black Mirror. The End")
+  time.sleep(2)
+  quit()
